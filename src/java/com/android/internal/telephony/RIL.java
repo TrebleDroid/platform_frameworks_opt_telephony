21d4d870565609b9a83dca6298a831f00772e330
Pierre-Hugues Husson <phh@phh.me>
2024-02-21 16:21:51 -0500

    Initialize Samsung HIDL ISehRadio. Fixes 5G on S20 Ultra

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -807,6 +807,7 @@ public class RIL extends BaseCommands implements CommandsInterface {
                 return null;
             } else {
                 IBinder binder;
+                serviceProxy.setServiceName(HIDL_SERVICE_NAME[mPhoneId]);
                 switch (service) {
                     case HAL_SERVICE_DATA:
                         if (mMockModem == null) {
@@ -865,7 +866,7 @@ public class RIL extends BaseCommands implements CommandsInterface {
                             mHalVersion.put(service, ((RadioNetworkProxy) serviceProxy).setAidl(
                                     mHalVersion.get(service),
                                     android.hardware.radio.network.IRadioNetwork.Stub
-                                            .asInterface(binder), HIDL_SERVICE_NAME[mPhoneId]));
+                                            .asInterface(binder)));
                         }
                         break;
                     case HAL_SERVICE_SIM:
